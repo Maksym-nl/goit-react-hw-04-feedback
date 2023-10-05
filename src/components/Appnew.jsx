7608638f06845cee20394892cad5a1eb0d487169
maksymnl <adw0988245131@gmail.com>
2023-10-05 09:50:47 +0300

    1

---
+++ src/components/Appnew.jsx
@@ -0,0 +1,62 @@
+// import React, { Component } from 'react';
+import { Feedback } from './Feedback/Feedback';
+import { Statistics } from './Statistics/Statistic';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+import { useState } from 'react';
+
+export const App = () => {
+  const [good, setGood] = useState([0]);
+  const [neutral, setNeutral] = useState([0]);
+  const [bad, setBad] = useState([0]);
+
+  const handleClick = () => option => {
+    switch (option) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const countTotalFeedback = () => {
+    // const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return Math.ceil((this.state.good / total) * 100) || 0;
+  };
+
+  //   const { good, neutral, bad } = state;
+  const total = countTotalFeedback();
+  const positiveFeedbackPercentage = countPositiveFeedbackPercentage();
+
+  return (
+    <>
+      <Section title="Feedback">
+        <Feedback handleClick={handleClick} options={Object.keys(this.state)} />
+      </Section>
+      <Section title="Statistics">
+        {total === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positiveFeedbackPercentage={positiveFeedbackPercentage}
+          />
+        )}
+      </Section>
+    </>
+  );
+};
