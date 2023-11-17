d7cf905a22c6e96ed984f025ce57265a34cbb78d
maksymnl <adw0988245131@gmail.com>
2023-11-17 09:29:09 +0200

    реакт 90 дз4

--- src/components/Appnew.jsx
+++ src/components/Appnew.jsx
@@ -6,11 +6,11 @@ import { Notification } from './Notification/Notification';
 import { useState } from 'react';
 
 export const Appnew = () => {
-  const [good, setGood] = useState([0]);
-  const [neutral, setNeutral] = useState([0]);
-  const [bad, setBad] = useState([0]);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const handleClick = () => option => {
+  const handleClick = option => {
     switch (option) {
       case 'good':
         setGood(prevState => prevState + 1);
